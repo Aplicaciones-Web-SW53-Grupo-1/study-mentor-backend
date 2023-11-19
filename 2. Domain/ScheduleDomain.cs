d2d1e92f511f59398a3c852c6ff4315d0b17f6ba
Tzabito <miguel_anxel@hotmail.com>
2023-11-19 16:51:37 -0500

    fix:update database and post

--- 2. Domain/ScheduleDomain.cs
+++ 2. Domain/ScheduleDomain.cs
@@ -1,12 +1,29 @@
 using _3._Data;
+using _3._Data.Model;
 
 namespace _2._Domain;
 
 public class ScheduleDomain : IScheduleDomain
 {
     private IScheduleData _scheduleData;
+
+    public ScheduleDomain(IScheduleData scheduleData)
+    {
+        _scheduleData = scheduleData;
+    }
     
-    
-    
+    public bool Create(Schedule schedule)
+    {
+        var existingSchedule = _scheduleData.GetByTitle(schedule.Title);
+
+        if (existingSchedule == null)
+        {
+            return _scheduleData.Create(schedule);
+        }
+        else
+        {
+            return false;
+        }
+    }
     
 }
